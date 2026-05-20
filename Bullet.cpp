22ea2a9c88cbc5e4d3dd35c98ef641397dfffaa9
ひらり 野崎 <250108@jc-21.jp>
2026-05-20 12:15:45 +0900

    金魚丸（スペースシップ）を出すことに成功したよ

---
+++ Bullet.cpp
@@ -0,0 +1,22 @@
+#include "Bullet.h"
+
+Bullet::Bullet(GameObject* parent)
+	:GameObject(parent, "Bullet"), hModel_(-1)
+{
+}
+
+void Bullet::Initialize()
+{
+}
+
+void Bullet::Update()
+{
+}
+
+void Bullet::Draw()
+{
+}
+
+void Bullet::Release()
+{
+}
--- PlayScene.cpp
+++ PlayScene.cpp
@@ -1,6 +1,6 @@
 #include "PlayScene.h"
 #include "Engine/Model.h"
-
+#include "Player.h"
 PlayScene::PlayScene(GameObject* parent)  //メンバの設定はこっち
 	:GameObject(parent, "PlayScene"), hModel_(-1)
 {
@@ -10,18 +10,20 @@ void PlayScene::Initialize() //その他はこっち
 {
 	hModel_ = Model::Load("Oden.fbx");
 	assert(hModel_ >= 0);
+	Instantiate<Player>(this);
 }
 
 void PlayScene::Update()
 {
 	static float time = 0.0f;
 
-	ot_.position_ = { 0.0f, 0.0f, 0.0f };
-	ot_.scale_ = { 1.0f, 1.0f, 1.0f };
+	ot_.position_ = { 0.0f, 0.0f, 10.0f };
+	ot_.scale_ = { 0.5f, 0.5f, 0.5f };
 	ot_.rotate_.y += 0.1f;
-	time += 0.1f;
-	float posx = 6.0 * sin(0.2f*time);
-	ot_.position_.x = posx;
+	time += 0.025f;
+	ot_.position_.x = 6.0f * sin(time);
+	//float posx = 6.0 * sin(0.2f*time);
+	//ot_.position_.x = posx;
 }
 
 void PlayScene::Draw()
